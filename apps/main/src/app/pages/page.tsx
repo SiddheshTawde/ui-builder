e56e8c9a7ba68981e25ffb0dd0ec5fd6ea6fd3f5
Siddhesh Tawde <siddheshtawde35@gmail.com>
2024-07-09 01:44:33 +0530

    Clean Up

--- apps/main/src/app/pages/page.tsx
+++ apps/main/src/app/pages/page.tsx
@@ -1,15 +1,7 @@
-import data from "./sample.json";
-import { DataTable } from "@root/components/core/data-table";
-import { columns } from "./columns";
-
 export default function Pages() {
   return (
     <main className="container mx-auto h-[calc(100vh-64px)]">
-      <DataTable
-        columns={columns}
-        data={data}
-        add={{ title: "Add New Page", href: "/pages/add" }}
-      />
+      Show all availabele Pages here.
     </main>
   );
 }
