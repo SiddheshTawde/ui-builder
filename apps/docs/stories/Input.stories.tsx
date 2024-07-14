a04ebe4b45799c48cc58316e0490e3916003908e
Siddhesh Tawde <siddheshtawde35@gmail.com>
2024-07-14 11:59:28 +0530

    Added font to story books

--- apps/docs/stories/Input.stories.tsx
+++ apps/docs/stories/Input.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Input } from "@repo/ui-components";
 import "@repo/ui-components/dist/index.css";
+import "./index.css";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
